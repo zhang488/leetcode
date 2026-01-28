2059bf95fad9671a350dd319d2467e6773084037
zsw <3256741559@qq.com>
2026-01-28 23:59:39 +0800

    Demo28x的平方根.java

--- 数组/Demo27将有序数组转换为二叉搜索树.java
+++ 数组/Demo27将有序数组转换为二叉搜索树.java
@@ -10,6 +10,11 @@ public class Demo27将有序数组转换为二叉搜索树 {
         return helper(nums, 0, nums.length - 1);
     }
 
+
+
+
+
+
     public TreeNode helper(int[] nums, int left, int right) {
         if (left > right) {
             return null;
---
+++ 数组/Demo28x的平方根.java
@@ -0,0 +1,12 @@
+package 数组;
+
+public class Demo28x的平方根 {
+
+    public int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        int ans = (int) Math.exp(0.5 * Math.log(x));
+        return (long) (ans + 1) * (ans + 1) <= x ? ans + 1 : ans;
+    }
+}

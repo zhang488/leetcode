01c703fa0f61594c3a6fb0563c579cf8b26ae78b
zsw <3256741559@qq.com>
2026-01-30 23:55:09 +0800

    Demo30位1的个数.java

---
+++ 数组/Demo30位1的个数.java
@@ -0,0 +1,15 @@
+package 数组;
+
+public class Demo30位1的个数 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int hammingWeight(int n) {
+        if(n <= 2) return 1;
+        else{
+            return hammingWeight(n/2) + n%2;
+        }
+    }
+}

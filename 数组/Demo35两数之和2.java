34f3c43480984e92dde76a1fab97ee82279e6b12
zsw <3256741559@qq.com>
2026-02-07 00:00:01 +0800

    Demo35两数之和2.java

---
+++ 数组/Demo35两数之和2.java
@@ -0,0 +1,23 @@
+package 数组;
+
+import java.util.Arrays;
+
+public class Demo35两数之和2 {
+
+    public int[] twoSum(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; ++i) {
+            int low = i + 1, high = numbers.length - 1;
+            while (low <= high) {
+                int mid = (high - low) / 2 + low;
+                if (numbers[mid] == target - numbers[i]) {
+                    return new int[]{i + 1, mid + 1};
+                } else if (numbers[mid] > target - numbers[i]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}

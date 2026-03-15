e6ab07c0f5f27a9a11afd2fce6fa67abf9de6d74
zsw <3256741559@qq.com>
2026-03-15 23:56:39 +0800

    Demo45实现一个Map.java

--- 数组/Demo45实现一个Map.java
+++ 数组/Demo45实现一个Map.java
@@ -1,5 +1,7 @@
 package 数组;
 
+import java.util.*;
+
 /**
  * 380. O(1) 时间插入、删除和获取随机元素
  * 已解答
@@ -48,6 +50,43 @@ public class Demo45实现一个Map {
 
     }
 
+    List<Integer> nums;
+    Map<Integer,Integer> indices;
+    Random random;
 
-    
+    public Demo45实现一个Map() {
+        nums=new ArrayList<>();
+        indices=new HashMap<>();
+        random=new Random();
+    }
+
+    public boolean insert(int val) {
+        if (indices.containsKey(val)) {
+            return false;
+        }
+        int index = nums.size();
+        nums.add(val);
+        indices.put(val, index);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!indices.containsKey(val)) {
+            return false;
+        }
+        int index = indices.get(val);
+        int last = nums.get(nums.size() - 1);
+        nums.set(index, last);
+        indices.put(last, index);
+        nums.remove(nums.size() - 1);
+        indices.remove(val);
+        return true;
+    }
+
+    public int getRandom() {
+        int randomIndex = random.nextInt(nums.size());
+        return nums.get(randomIndex);
+    }
+
+
 }

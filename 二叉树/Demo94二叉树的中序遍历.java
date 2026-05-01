686ceb6d7a59d88bfb013db07cd55000c62400f2
zsw <3256741559@qq.com>
2026-05-01 23:56:40 +0800

    Demo94二叉树的中序遍历.java

---
+++ 二叉树/Demo94二叉树的中序遍历.java
@@ -0,0 +1,62 @@
+package 二叉树;
+
+import 数组.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 94. 二叉树的中序遍历
+ * 已解答
+ * 简单
+ * 相关标签
+ * premium lock icon
+ * 相关企业
+ * 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ * 示例 2：
+ *
+ * 输入：root = []
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：root = [1]
+ * 输出：[1]
+ *
+ *
+ * 提示：
+ *
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 <= Node.val <= 100
+ *
+ *
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ */
+public class Demo94二叉树的中序遍历 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        inorder(root, res);
+        return res;
+    }
+
+    public void inorder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+}
--- 数组/TreeNode.java
+++ 数组/TreeNode.java
@@ -1,7 +1,7 @@
 package 数组;
 
 public class TreeNode{
-    int val;
+    public int val;
     public TreeNode left;
     public TreeNode right;
     TreeNode(){};

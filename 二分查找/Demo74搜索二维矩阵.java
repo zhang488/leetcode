93c593bd5f54172cdabac333f30dd9f428a597bd
zsw <3256741559@qq.com>
2026-05-18 23:51:29 +0800

    Demo74搜索二维矩阵.java

---
+++ 二分查找/Demo74搜索二维矩阵.java
@@ -0,0 +1,53 @@
+package 二分查找;
+
+/**
+ * 74. 搜索二维矩阵
+ * 已解答
+ * 中等
+ * 相关标签
+ * premium lock icon
+ * 相关企业
+ * 给你一个满足下述两条属性的 m x n 整数矩阵：
+ *
+ * 每行中的整数从左到右按非严格递增顺序排列。
+ * 每行的第一个整数大于前一行的最后一个整数。
+ * 给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+ * 输出：true
+ * 示例 2：
+ *
+ *
+ * 输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+ * 输出：false
+ */
+public class Demo74搜索二维矩阵 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int low = 0, high = m * n - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int x = matrix[mid / n][mid % n];
+            if (x < target) {
+                low = mid + 1;
+            } else if (x > target) {
+                high = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
